67b559dc7c441855c50f02837702c761f8f3c866
Fluflu64 <matisriou360@gmail.com>
2025-03-13 13:52:40 +0100

    0.6 update bobcity
    ajout d'interaction dans la ville

--- player_related/script_interactable.gd
+++ player_related/script_interactable.gd
@@ -2,7 +2,13 @@ class_name Interactable
 extends Area2D
 
 @export var lines = ["oui","oui"]
+@export var hide:bool = true
 @onready var animation = $AnimationPlayer
+@onready var sprite = $Sprite2D
+
+func _ready():
+	if hide :
+		sprite.hide()
 
 func end_talk():
 	print("oui")

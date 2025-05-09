6ac8537cee9d2475504629b1b847db797d726618
Fluflu64 <matisriou360@gmail.com>
2025-05-09 19:02:17 +0200

    beta 0.12.3

--- actor/script_push_block.gd
+++ actor/script_push_block.gd
@@ -1,19 +1,44 @@
-extends RigidBody2D
+extends StaticBody2D
 
-func push(game_root):
-	var player_pos = Vector2(snappedi(BobGlobal.game_root.player.position.x+8,16),snappedi(BobGlobal.game_root.player.position.y-16,16))-Vector2(8,-24)
-	var my_pos = Vector2(snappedi(position.x+8,16),snappedi(position.y-16,16))-Vector2(8,-8)
-	var tween = create_tween()
-	var new_pos = sign(player_pos - my_pos)*-32
-	tween.tween_property(self,"linear_velocity",new_pos,0.5)
-	await tween.finished
-	linear_velocity = Vector2.ZERO
+@onready var col_left = $left
+@onready var col_right = $right
+@onready var col_up = $up
+@onready var col_down = $down
+@onready var raycast = $ray
+@onready var animation = $AnimationPlayer
+@onready var dust = $GPUParticles2D
 
-func _process(delta: float) -> void:
-	var player_pos = Vector2(snappedi(BobGlobal.game_root.player.position.x+8,16),snappedi(BobGlobal.game_root.player.position.y-16,16))-Vector2(8,-24)
-	var my_pos = Vector2(snappedi(position.x+8,16),snappedi(position.y-16,16))-Vector2(8,-8)
-	var new_pos = position - (player_pos - my_pos)
-	$Sprite2D2.global_position = new_pos
+func push(_game_root):
+	var new_pos = position
+	
+	for body in col_left.get_overlapping_bodies() :
+		if body is Player :
+			new_pos += Vector2(16,0)
+			raycast.target_position = Vector2(16,0)
+	
+	for body in col_right.get_overlapping_bodies() :
+		if body is Player :
+			new_pos += Vector2(-16,0)
+			raycast.target_position = Vector2(-16,0)
+	
+	for body in col_up.get_overlapping_bodies() :
+		if body is Player :
+			new_pos += Vector2(0,16)
+			raycast.target_position = Vector2(0,16)
+	
+	for body in col_down.get_overlapping_bodies() :
+		if body is Player :
+			new_pos += Vector2(0,-16)
+			raycast.target_position = Vector2(0,-16)
+	animation.play("start_push")
+	await animation.animation_finished
+	
+	if not raycast.is_colliding() :
+		dust.emitting = true
+		var tween = create_tween()
+		tween.tween_property(self,"position",new_pos,0.5)
+		await tween.finished
+		dust.emitting = false
 
 func function(game_root,n):
 	if n == 0 :
